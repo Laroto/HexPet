9c24c5daeef7a4b3e4cc2eee0a788774651a773c
Laroto <up201806660@fe.up.pt>
2022-10-05 13:03:58 +0100

    working test with a single joint

---
+++ src/single_joint.cpp
@@ -0,0 +1,37 @@
+#include <ros/ros.h>
+
+#include <std_msgs/Float64.h>
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "teste_joints");
+
+    ros::NodeHandle nh;
+    ros::Publisher pub;
+
+    pub = nh.advertise<std_msgs::Float64>("/hexpet/femur_joint_LM_position_controller/command", 1);
+
+    std_msgs::Float64 msg;
+
+    bool flag = true;
+
+    ros::Rate loop_rate(1);
+    while (ros::ok())
+    {
+        if (flag == true)
+        {  
+            msg.data = 0.0;
+            flag = false;
+        }
+        else
+        {       
+            msg.data = -1.57;
+            flag = true;
+        }
+
+        pub.publish(msg);
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+
+}
